e3ed49ecdc9e1225233bef6f87def55788b772cf
M4RC02U1F4A4 <23193188+M4RC02U1F4A4@users.noreply.github.com>
2022-06-12 11:10:27 +0200

    modified dashboard

--- web/app/server.py
+++ web/app/server.py
@@ -1,20 +1,43 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import os
 import pymongo
 
 app = Flask(__name__)
 
-
-
 @app.route('/')
 def home():
+    return redirect("/published", 301)
+
+@app.route('/published')
+def published():
     mydict = stats.find_one({"_id":"stats"})
     recent_CRITICAL = cveDB.find(({'baseScore':  {'$gte': 9.0}})).sort('publishedDate', -1).limit(10)
     recent_HIGH = cveDB.find({'baseScore':  {'$gte': 7.0, '$lte': 8.9}}).sort('publishedDate', -1).limit(10)
     recent_MEDIUM = cveDB.find({'baseScore':  {'$gte': 4.0, '$lte': 6.9}}).sort('publishedDate', -1).limit(10)
     recent_LOW = cveDB.find({'baseScore': {'$gte': 0.1, '$lte': 3.9}}).sort('publishedDate', -1).limit(10)
     return render_template(
-        'home.html',
+        'published.html',
+        numberOfCVE = mydict['numberOfCVE'],
+        numberOfCVE_CRITICAL = mydict['numberOfCVE_CRITICAL'],
+        numberOfCVE_HIGH = mydict['numberOfCVE_HIGH'],
+        numberOfCVE_MEDIUM = mydict['numberOfCVE_MEDIUM'],
+        numberOfCVE_LOW = mydict['numberOfCVE_LOW'],
+        numberOfCVE_NoScore = mydict['numberOfCVE_NoScore'],
+        recent_CRITICAL = recent_CRITICAL,
+        recent_HIGH = recent_HIGH,
+        recent_MEDIUM = recent_MEDIUM,
+        recent_LOW = recent_LOW,
+    )
+
+@app.route('/modified')
+def modified():
+    mydict = stats.find_one({"_id":"stats"})
+    recent_CRITICAL = cveDB.find(({'baseScore':  {'$gte': 9.0}})).sort('lastModifiedDate', -1).limit(10)
+    recent_HIGH = cveDB.find({'baseScore':  {'$gte': 7.0, '$lte': 8.9}}).sort('lastModifiedDate', -1).limit(10)
+    recent_MEDIUM = cveDB.find({'baseScore':  {'$gte': 4.0, '$lte': 6.9}}).sort('lastModifiedDate', -1).limit(10)
+    recent_LOW = cveDB.find({'baseScore': {'$gte': 0.1, '$lte': 3.9}}).sort('lastModifiedDate', -1).limit(10)
+    return render_template(
+        'modified.html',
         numberOfCVE = mydict['numberOfCVE'],
         numberOfCVE_CRITICAL = mydict['numberOfCVE_CRITICAL'],
         numberOfCVE_HIGH = mydict['numberOfCVE_HIGH'],

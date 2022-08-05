b42d8c352353416147205418c11f08b01d4899d5
M4RC02U1F4A4 <23193188+M4RC02U1F4A4@users.noreply.github.com>
2022-08-05 20:04:31 +0200

    Disable debug for Flask

--- web/app/server.py
+++ web/app/server.py
@@ -240,4 +240,4 @@ if __name__ == '__main__':
     kev = db['kev']
     pTuesday = db['pTuesday']
 
-    app.run(debug=True, port=5000, host='0.0.0.0')
+    app.run(debug=False, port=5000, host='0.0.0.0')
